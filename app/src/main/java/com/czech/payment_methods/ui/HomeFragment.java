378de8ecfd7e5d318378da9fd6ee0296e0ba3a71
Ezichi Amarachi <ezichukwuamarachi@gmail.com>
2021-12-10 07:03:10 +0100

    added view binding

--- app/src/main/java/com/czech/payment_methods/ui/HomeFragment.java
+++ app/src/main/java/com/czech/payment_methods/ui/HomeFragment.java
@@ -2,19 +2,36 @@ package com.czech.payment_methods.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.czech.payment_methods.R;
+import com.czech.payment_methods.databinding.FragmentHomeBinding;
 
 public class HomeFragment extends Fragment {
 
+    FragmentHomeBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentHomeBinding.inflate(getLayoutInflater());
+
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        binding.toPaymentMethods.setOnClickListener(it ->
+                Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_paymentListFragment)
+        );
     }
 }
--- app/src/main/java/com/czech/payment_methods/ui/PaymentListFragment.java
+++ app/src/main/java/com/czech/payment_methods/ui/PaymentListFragment.java
@@ -2,20 +2,23 @@ package com.czech.payment_methods.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.czech.payment_methods.R;
+import com.czech.payment_methods.databinding.FragmentPaymentListBinding;
 
 public class PaymentListFragment extends Fragment {
 
+    FragmentPaymentListBinding binding;
+
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_payment_list, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        binding = FragmentPaymentListBinding.inflate(getLayoutInflater());
+
+        return binding.getRoot();
     }
 }
